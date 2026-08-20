025e04f42711d1c3ef63722c65b8ba1dce2787b9
Mac <mac@kariz.local>
2026-08-20 16:47:03 +0330

    rebuild sale catalog and checkout flow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'haleh-crm-v25-pages-4';
+const CACHE = 'haleh-crm-v25-pages-5';
 const BASE_URL = new URL('./', self.registration.scope);
 const INDEX_URL = new URL('index.html', BASE_URL).href;
 const APP_SHELL = [
