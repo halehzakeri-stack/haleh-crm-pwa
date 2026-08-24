321014fa48e4ed0305c217e7d3ea09207eb89a69
Mac <mac@kariz.local>
2026-08-24 13:54:36 +0330

    Redesign mobile navigation around mezon hub

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'haleh-crm-v27-pages-21';
+const CACHE = 'haleh-crm-v28-pages-22';
 const BASE_URL = new URL('./', self.registration.scope);
 const INDEX_URL = new URL('index.html', BASE_URL).href;
 const APP_SHELL = [
