08cb3aa36a56816f19afb1c5fe721ef6b76613a7
Mac <mac@kariz.local>
2026-08-30 17:07:15 +0330

    Add recoverable customer deletion

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'haleh-crm-v32-icon-render';
+const CACHE = 'haleh-crm-v33-customer-trash';
 const BASE_URL = new URL('./', self.registration.scope);
 const INDEX_URL = new URL('index.html', BASE_URL).href;
 const APP_SHELL = [
