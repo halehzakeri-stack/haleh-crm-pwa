aff4ed532fc08f02c66aae6f855ecce93d9810cf
Mac <mac@kariz.local>
2026-08-30 17:39:01 +0330

    Route customer back button to home

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'haleh-crm-v35-year-format';
+const CACHE = 'haleh-crm-v36-customer-home-back';
 const BASE_URL = new URL('./', self.registration.scope);
 const INDEX_URL = new URL('index.html', BASE_URL).href;
 const APP_SHELL = [
