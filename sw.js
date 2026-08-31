1648b72b5d6608256f9bf4735a30a184a5caedc2
Mac <mac@kariz.local>
2026-08-31 10:10:59 +0330

    Remove category placeholder border

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'haleh-crm-v46-category-divider-cleanup';
+const CACHE = 'haleh-crm-v47-category-divider-cleanup';
 const BASE_URL = new URL('./', self.registration.scope);
 const INDEX_URL = new URL('index.html', BASE_URL).href;
 const APP_SHELL = [
