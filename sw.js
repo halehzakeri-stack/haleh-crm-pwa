8f01e0ede3a3e350f7e822fb10094b9cd27911bd
Mac <mac@kariz.local>
2026-09-26 22:52:14 +0330

    feat: add production order measurements

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'haleh-crm-v89-partner-home-icon';
+const CACHE = 'haleh-crm-v90-production-measurements';
 const BASE_URL = new URL('./', self.registration.scope);
 const INDEX_URL = new URL('index.html', BASE_URL).href;
 const APP_SHELL = [
